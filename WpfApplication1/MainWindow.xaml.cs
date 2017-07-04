4944a18f53376031a5165cc0c920eb5ce743ab10
evgeniasemez <evgenia.semez@gmail.com>
2017-07-03 21:34:35 -0700

    Initial Commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml; 
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static URLCache urlCache;
+
+        public MainWindow()
+        {         
+            InitializeComponent();
+            // instanciate url cache
+            urlCache = new URLCache();
+            // load all created TinyUrls from the database
+            urlCache.LoadURLs();
+            DisplayResults();
+        }
+
+        private void DisplayResults()
+        {
+            string myDocuments = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string file = String.Format("{0}\\urlcache.txt", myDocuments);
+
+            if (File.Exists(file))
+            { 
+                StreamReader reader = new StreamReader(file);
+
+                TextBox tb = this.Results;
+                tb.Clear();
+                tb.Margin = new Thickness(10);
+                tb.TextWrapping = TextWrapping.Wrap;
+                string displayResult = reader.ReadToEnd();
+                tb.AppendText(displayResult);  // display the result in the text box
+
+                reader.Close();
+            }
+        }
+        // Hide browser button
+        private void HideMainBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserPopup.IsOpen = false;
+        }
+        // click on the Navigate button
+        private void Navigate_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserPopup.IsOpen = true;
+            // get the long URL to navigate from the cache
+            string urlToNavigate = urlCache.GetUrlCache()[NavigateToUrl.Text.Trim()];
+            MainBrowser.Navigate(urlToNavigate);
+        }
+        // click on Create TinyUrl button
+        private void CreateTinyUrl_Click(object sender, RoutedEventArgs e)
+        {
+            string tinyUrl = urlCache.AddURL(URL.Text);
+
+            if (tinyUrl == "")
+            {
+                MessageBox.Show("Incorrect or empty URL is entered");
+            }
+            else if (tinyUrl == "TinyUrl is already created")
+            {
+                MessageBox.Show("TinyUrl is already created");
+            }
+            else
+            {
+                DisplayResults();
+            }
+        }
+    }
+}
---
+++ WpfApplication1/URLCache.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class URLCache
+    {
+        // number of characters we are going to use in the TinyURL
+        private const int size = 7;
+        // storage for URLs
+        private Dictionary<string, string> urlCache;
+        // initialize random variable
+        private static Random random = new Random((int)DateTime.Now.Ticks);
+        // key is TinyUrl and value is long Url 
+        public URLCache()
+        {
+            urlCache = new Dictionary<string, string>();
+        }
+
+        public Dictionary<string, string> GetUrlCache()
+        {
+            return urlCache;
+        }
+
+        // function to create TinyUrl and return the result
+        public string AddURL(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                return "";
+            }
+            // checking if url starts with anything but http
+            if (url.StartsWith("http") == false)
+            {
+                return "";
+            }
+            // checking if url is already created
+            if (urlCache.ContainsValue(url.Trim()) == true)
+            {
+                return "TinyUrl is already created";
+            }
+            // perform in a loop for 1000 times until non duplicated TinyUrl is created. But getting a duplicated TynyUrl is almost impossible.
+            // its going to the loop only if there is a duplicate
+            for(int i=0; i<1000; i++)
+            {
+                string randomString = CreateRandomString();
+                if (urlCache.ContainsKey(randomString) == false)
+                {
+                    string tinyUrl = String.Format("http://EvgeniaTinyUrl.com/{0}", randomString);
+                    urlCache.Add(tinyUrl, url);
+                    AppendToFile(tinyUrl, url);
+                    return randomString;
+                }
+            }
+
+            return "";
+        }
+        // adding urls to the database which is just a file  
+        public void AppendToFile(string tinyURL, string URL)
+        {
+            string myDocuments = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string file = String.Format("{0}\\urlcache.txt", myDocuments);
+
+            StreamWriter sw = new StreamWriter(new FileStream(file, FileMode.Append, FileAccess.Write), Encoding.UTF8);
+            // write comma seperated line
+            sw.WriteLine(String.Format("{0}  ,  {1}", tinyURL, URL)); 
+            sw.Flush(); 
+            sw.Close();
+        }
+        // load all created urls from the file to the memory(urlcache)
+        public void LoadURLs()
+        {
+            string myDocuments = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string fileName = String.Format("{0}\\urlcache.txt", myDocuments);
+
+            // read all the lines
+            if (File.Exists(fileName))
+            { 
+                string[] lines = File.ReadAllLines(fileName);
+
+                foreach(string line in lines)
+                {
+                    // parse and load
+                    string[] items = line.Split(',');
+                    if (items.Length == 2)
+                    {
+                        urlCache.Add(items[0].Trim(), items[1].Trim());
+                    }
+                }
+            }
+        }
+        // creating a random string of lenght size(7)
+        public string CreateRandomString()
+        {
+            string input = "abcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = input[random.Next(0, input.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+    }
+}
